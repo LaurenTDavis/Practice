39a6ec6d6fb7fc6c10a3b63ebd3b02b07fd71051
Student <student@RU-009.local>
2015-09-21 11:06:56 -0600

    prototype practice

--- main.js
+++ main.js
@@ -5,35 +5,55 @@
 // 3. console log the value of the instance 
 // bonus : have defualt value if text/author are not passed in
 
-// var Quote = function(text, author) {
-// 	this.text = text || "Default Text" ;
-// 	this.author = author || "Default Author";
+
+
+
+
+// var Iguana = function(name, color, bulgyEyes, tubbiness) {
+// 	this.name = name;
+// 	this.color = color;
+// 	this.bulgyEyes = bulgyEyes;
+// 	this.tubbiness = tubbiness; 
+// 	// this.eat = function() {
+// 	// 	console.log(this.name + " is eating. Om nom nom..."); 
+// 	// }
+// }
+// var chubChub = new Iguana('ChubChub', 'Aquamarine', true, true);
+// console.log(chubChub); 
+
+// var herbert = new Iguana('Herbert', 'Brown', false, true); 
+// console.log(herbert);
+
+// Iguana.prototype.eat = function() {
+// 	console.log(this.name + " is eating. yummy"); 
 // }
 
-// var someQuote = new Quote("So long and thanks for all the fish", "Douglas Adams");
-// console.log(someQuote);
 
-// var thatQ = new Quote();
-// console.log(thatQ);
-
-
+// 	CHALLENGE
+// Update Quote Class to have 2 methods on its prototype
+// a. method to count the number of words in the  quote
+//	b. method to console log an uppercased version of the quote with exclamation points 
 
 
-var Iguana = function(name, color, bulgyEyes, tubbiness) {
-	this.name = name;
-	this.color = color;
-	this.bulgyEyes = bulgyEyes;
-	this.tubbiness = tubbiness; 
-	// this.eat = function() {
-	// 	console.log(this.name + " is eating. Om nom nom..."); 
-	// }
+var Quote = function(text, author) {
+	this.text = text || "Default Text" ;
+	this.author = author || "Default Author";
 }
-var chubChub = new Iguana('ChubChub', 'Aquamarine', true, true);
-console.log(chubChub); 
 
-var herbert = new Iguana('Herbert', 'Brown', false, true); 
-console.log(herbert);
+var someQuote = new Quote("So long and thanks for all the fish", "Douglas Adams");
+console.log(someQuote);
 
-Iguana.prototype.eat = function() {
-	console.log(this.name + " is eating. yummy"); 
+var thatQ = new Quote();
+console.log(thatQ);
+
+
+
+Quote.prototype.count = function() {
+	return this.text.split(' ').length;
 }
+
+Quote.prototype.excite = function() {
+	console.log(this.text.toUpperCase() + '!!!');
+}
+
+
